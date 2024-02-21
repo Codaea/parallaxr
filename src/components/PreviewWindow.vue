4e7f576b706e8fd610da5d7237cc50f459dd6c0c
Codaea <dakotajayroth@gmail.com>
2024-02-20 21:51:48 -0800

    *basic implementation of tiling canvas

---
+++ src/components/PreviewWindow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Application, onTick, Loader } from 'vue3-pixi'
+
+
+
+</script>
+
+<template>
+    <h1>Preview Window</h1>
+    <Application :width="700" :height="700"> <!-- TODO: form to screen-->
+    <tiling-sprite
+    texture='/preview.png'
+      :anchor="0.5"
+      :x="0"
+      :y="0"
+      :width="1920"
+      :height="1080"
+    />
+    </Application>
+</template>
--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PreviewWindowVue from '../components/PreviewWindow.vue';
+
 
 const images = ref([])
-
 </script>
 
 <template>
   <div class="editor">
     <h1>This is an editor page</h1>
     <div class="toolbar">
-      
       <h2>Layers</h2>
       <ul>
         <li>open image(s)</li>
@@ -17,10 +17,10 @@ const images = ref([])
       <ul>
         <li v-for="image in images" :key="image">Image</li>
       </ul>
-
     </div>
     <div class="preview_render">
       <h2>Preview</h2>
+        <PreviewWindowVue/>
     </div>
   </div>
 </template>
