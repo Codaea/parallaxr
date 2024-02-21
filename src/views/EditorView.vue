0e7e626f9c2861e99f701dbc7123a5196f63f1bc
Codaea <dakotajayroth@gmail.com>
2024-02-20 16:45:42 -0800

    Starting EditorView As a large monofile *decoupling code later

---
+++ src/views/EditorView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const images = ref([])
+
+</script>
+
+<template>
+  <div class="editor">
+    <h1>This is an editor page</h1>
+    <div class="toolbar">
+      
+      <h2>Layers</h2>
+      <ul>
+        <li>open image(s)</li>
+      </ul>
+      <ul>
+        <li v-for="image in images" :key="image">Image</li>
+      </ul>
+
+    </div>
+    <div class="preview_render">
+      <h2>Preview</h2>
+    </div>
+  </div>
+</template>
